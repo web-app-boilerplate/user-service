2eef5e2c92320619638891a7dc94902be871a7b8
Mustapha SMAIL <mustapha.smail@parisnanterre.fr>
2025-09-26 10:01:24 +0200

    update migrations

--- prisma/migrations/20250926071919_init/migration.sql
+++ prisma/migrations/20250926071919_init/migration.sql
@@ -2,9 +2,13 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
-    "isPaid" BOOLEAN NOT NULL DEFAULT false,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "credit" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "refreshToken" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
